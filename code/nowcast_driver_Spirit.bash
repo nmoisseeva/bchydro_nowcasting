2297bab55f8e99e3a0bfff520b22c1aaa036c21c
Nadya Moisseeva <nmoisseeva@eos.ubc.ca>
2016-02-18 12:39:39 -0800

    update spirit driver

--- code/nowcast_driver_Spirit.bash
+++ code/nowcast_driver_Spirit.bash
@@ -5,7 +5,7 @@
 #nmoisseeva@eos.ubc.ca
 
 #--------------------------input-------------------------------
-fcst_drive="/Volumes/forecasts/WRF3_RAW"
+fcst_drive="/nfs/asiaq/forecasts/WAN00WP03"
 #-----------------------end of input---------------------------
 
 
@@ -67,25 +67,25 @@ echo "Extracting observations from the EmWxNet database"
 echo "Moving obs station data to $emx_path"
 mv ./*.txt $emx_path > /dev/null	
  
-# #check if the necessary NetCDF file aready exists
-# netcdf_name=$netcdf_prefix$year-$month-$day"_"$hour:$fcst_init:00
-# old_netcdf_name=$netcdf_prefix$year-$month-$day"_"$old_hr:$fcst_init:00
-# netcdf_path="$netcdf_dir$year/$month/$day/$netcdf_name"
-# old_netcdf_path="$netcdf_dir$year/$month/$day/$old_netcdf_name"
-# if [ -e "$netcdf_path" ] && [ -e "$old_netcdf_path" ]; then
-# 	echo "Found existing NetCDF files at: $netcdf_path"
-# elif [ -e "$netcdf_path" ] && [ ! -e "$old_netcdf_path" ]; then
-# 	echo "Downloading supporting NetCDF model data from Asiaq to: $old_netcdf_path"
-# 	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$old_netcdf_name $old_netcdf_path
-# else
-# 	echo "Moving required NetCDF model data from Asiaq to: $netcdf_path"
-# 	mkdir -p $netcdf_dir$year/$month/$day/
-# 	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$netcdf_name $netcdf_path
-# 	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$old_netcdf_name $old_netcdf_path
-# fi
+#check if the necessary NetCDF file aready exists
+netcdf_name=$netcdf_prefix$year-$month-$day"_"$hour:$fcst_init:00
+old_netcdf_name=$netcdf_prefix$year-$month-$day"_"$old_hr:$fcst_init:00
+netcdf_path="$netcdf_dir$year/$month/$day/$netcdf_name"
+old_netcdf_path="$netcdf_dir$year/$month/$day/$old_netcdf_name"
+if [ -e "$netcdf_path" ] && [ -e "$old_netcdf_path" ]; then
+	echo "Found existing NetCDF files at: $netcdf_path"
+elif [ -e "$netcdf_path" ] && [ ! -e "$old_netcdf_path" ]; then
+	echo "Downloading supporting NetCDF model data from Asiaq to: $old_netcdf_path"
+	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$old_netcdf_name $old_netcdf_path
+else
+	echo "Moving required NetCDF model data from Asiaq to: $netcdf_path"
+	mkdir -p $netcdf_dir$year/$month/$day/
+	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$netcdf_name $netcdf_path
+	cp $fcst_drive/$yr$month$day$fcst_init/bsc-west/$old_netcdf_name $old_netcdf_path
+fi
 
-# echo "Initializing main Python routine for data assimilation"
-# mkdir -p $fig_dir/$year/$month/$day/$hour
+echo "Initializing main Python routine for data assimilation"
+mkdir -p $fig_dir/$year/$month/$day/$hour
 # python main.py $netcdf_name
 
 echo "=============================================================="
